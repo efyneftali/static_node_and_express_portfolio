cb7fa3f5094340067b5587f0f2af3bd501649e91
Efy Neftali Hernandez <efy.hernandez@gmail.com>
2022-07-08 11:10:37 -0500

    index and about routes working

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
 const express = require('express')
+
 const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-
+//const cookieParser = require('cookie-parser')
 const app = express();
+const {projects} = require('./projectData.json')
+//const { projects} = data
 
+//console.log(projects)
+console.log("break here")
+console.log(projects[0].image_urls)
+console.log(projects[1].project_name)
+app.use('/static', express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false}))
+
+//anything in the public folder will be routed to static
+
+app.use('/static', express.static('public'))
 app.set('view engine', 'pug')
 
-app.get('/', (req, res)=>{
-    res.send("<h1>ep im working</h1>")
-})
+app.get('/', (req, res,next) => {
+    res.locals.projects = projects;
+	res.render('index', projects)
+    //res.render('about')
+});
+
+// app.get('/project:id', (req, res,next) => {
+//     const id = req.params.id
+// 	res.locals.projects = projects[id]
+//     res.render('project',projects)
+// });
+app.get('/about', (req, res,next) => {
+    res.render('about')
+});
+
+// app.use((req,res,next) =>{
+// 	const err = new Error('Something went wrong');
+// 	res.locals.error = err;
+// 	err.status = 404;
+// 	res.render('error');
+// });
+
+// app.use((err, req, res, next) => {
+//     res.locals.error = err
+//     console.log(`An error has occured: ${err.status}`.red);
+//     res.render('error') 
+// });
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
---
+++ routes/project.js
@@ -0,0 +1,15 @@
+// const express = require('express')
+// const router = express.Router()
+// const { projects } = require('../projectData.json')
+
+// console.log(projects)
+// console.log(projects[0])
+// // router.get( '/', ( req, res ) => {
+// //   //const numberOfCards = cards.length;
+// //   //const flashcardId = Math.floor( Math.random() * numberOfCards );
+// //   //res.redirect( `/cards/${flashcardId}` )
+// //     //res.send(projects[0])
+// //     //res.send("<h1>ep im working</h1>")
+// // });
+
+// module.exports = router
