0505fc1cbb750060b695ff71b8f021f0621260ee
Efy Neftali Hernandez <efy.hernandez@gmail.com>
2022-07-09 13:20:58 -0500

    error is caught at the project route

--- app.js
+++ app.js
@@ -12,29 +12,45 @@ app.set('view engine', 'pug')
 app.get('/', (req, res) => {
     res.locals.projects = projects;
 	res.render('index', projects)
-});
+})
 
-app.get('/project/:id', (req, res) => {
+
+app.get('/project/:id', (req, res, next) => {
     const id = req.params.id
-	res.locals.projects = projects[id]
-    res.render('project')
+    if (projects[id] !== undefined) {
+    // render the project template if the id exist
+        res.locals.projects = projects[id]
+        res.render('project')
+    } else {
+        // otherwise, pass in error
+        const err = new Error();
+        err.message = "Well this is awkward.......Sorry, this project does not exist";
+        err.status = 404
+        next(err);
+    }
 });
 
 app.get('/about', (req, res,next) => {
     res.render('about')
     
 });
+
+//errors
 app.use((req,res,next) =>{
+    //creats 404 error
 	const err = new Error('Page Not Found')
     err.status = 404
     next(err)
 })
+
 app.use((err, req, res, next) => {
+    //handles other type of errors
     res.locals.error = err
-    res.status(err.status)
-    console.log(`An error has occured: ${err.status}`)
+    const status = err.status || 500
+    res.status(status)
+    console.log(`An error has occured: ${status}`)
     res.render('error') 
-});
+})
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
